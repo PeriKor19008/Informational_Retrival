ac2b1e2cf35ba77178d634771e33bbe140bfec92
peri <peri1908kor@gmail.com>
2023-11-25 13:01:57 +0200

    test

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from inverted_dictionary import dictionary_maker
 if __name__ == '__main__':
+    # if second arg on dictionary_maker is 1 stop words (and,or etc) will be filtered out
+    # if it is 0 then they will not
     D = dictionary_maker("docs",1)
     dict = D.dict
